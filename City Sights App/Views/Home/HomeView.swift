c803858cea3f092aae0bc40cd8a393ab92b1908e
cmsho <55258597+cmsho@users.noreply.github.com>
2021-09-28 19:36:10 -0400

    - Add BusinessMap - Add annotations to BusinessMap - Modify HomeView to toggle to map

--- City Sights App/Views/Home/HomeView.swift
+++ City Sights App/Views/Home/HomeView.swift
@@ -29,7 +29,9 @@ struct HomeView: View {
                             Image(systemName: "location")
                             Text("San Francisco")
                             Spacer()
-                            Text("Switch to map view")
+                            Button("Switch to map view") {
+                                self.isMapShowing = true
+                            }
                         }
                         
                         Divider()
@@ -42,6 +44,8 @@ struct HomeView: View {
                 }
                 else {
                     // TODO: Show map
+                    BusinessMap()
+                        .ignoresSafeArea()
                 }
             }
         }
